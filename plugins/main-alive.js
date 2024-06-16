492182666a5f7f8742ed222a2883fb16d63141ed
Romek XD <mdfaiyazraj786@gmail.com>
2024-06-16 17:03:28 +0530

    Update main-alive.js

--- plugins/main-alive.js
+++ plugins/main-alive.js
@@ -5,9 +5,9 @@ let handler = async(m, { conn, text, usedPrefix, command }) => {
     // Sound
     let name = m.pushName || conn.getName(m.sender)
     var vn = "./Assets/ALIVE.mp3"
-    let url = "https://github.com/abrahamdw882"
-    let murl = "https://YouTube.com"
-    let img = 'https://telegra.ph/file/35086802022652ed9a897.jpg"
+    let url = "https://github.com/Romeofaiz"
+    let murl = "https://github.com"
+    let img = "https://telegra.ph/file/94ee125634ca4dfe7b137.jpg"
     let con = { key: { fromMe: false, participant: `${m.sender.split`@`[0]}@s.whatsapp.net`, ...(m.chat ? { remoteJid: '919341378016@s.whatsapp.net' } : {}) }, message: { contactMessage: { displayName: `${name}`, vcard: `BEGIN:VCARD\nVERSION:3.0\nN:;a,;;;\nFN:${name}\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`}}}
     let doc = {
         audio: {
@@ -17,23 +17,23 @@ let handler = async(m, { conn, text, usedPrefix, command }) => {
         ptt: true,
         waveform:  [100, 0, 100, 0, 100, 0, 100],
         fileName: "Guru",
-    
+
         contextInfo: {
           mentionedJid: [m.sender],
           externalAdReply: {
-          title: "THE ROMEK-XD IS ALIVE 🌟",
-          body: "XLICON BOT",
+          title: "🌪ROMEK-XD IS ACTIVE🌪",
+          body: "ROMEK XD",
           thumbnailUrl: img,
-          sourceUrl: 'https://chat.whatsapp.com/FJuOAzoBSGcKgDXH23L9OH',
+          sourceUrl: 'https://whatsapp.com/channel/0029VaNdKNxK5cDKRztKXV3g',
           mediaType: 1,
           renderLargerThumbnail: true
           }}
       };
-    
+
       await conn.sendMessage(m.chat, doc, { quoted: con });
-    
+
     }
-    
+
     handler.help = ['alive']
     handler.tags = ['main']
     handler.command = /^(alive)$/i 
